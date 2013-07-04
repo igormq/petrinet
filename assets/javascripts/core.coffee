ff2469e2c4aa6401d1f34b2d1b8ce070f9d89155
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2013-07-04 17:23:44 -0300

    Adicionado o core

--- assets/javascripts/core.coffee
+++ assets/javascripts/core.coffee
@@ -3,12 +3,24 @@ $ ->
 
   paper = Raphael($("#main"), "100%", "100%")
 
-  circle = paper.circle(50, 40, 10)
+  circle = paper.circle(50, 40, 20)
 
-  circle.attr("fill", "#f00")
-
-
-  circle.attr("stroke", "#fff")
+  circle
+    .attr
+      fill: '#f00',
+      stroke: "#fff",
+      data:
+        fichas: 0
+      cursor: "pointer"
+    .drag(
+      (dx, dy) ->
+          @attr
+            cx: @ox + dx,
+            cy: @oy + dy
+      () ->
+        @ox = @attr("cx")
+        @oy = @attr("cy")
+    )
 
   $(window).resize () ->
     paper.setSize($(window).width(),$(window).height())
--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,9 +1,23 @@
 $(function() {
   var circle, paper;
   paper = Raphael($("#main"), "100%", "100%");
-  circle = paper.circle(50, 40, 10);
-  circle.attr("fill", "#f00");
-  circle.attr("stroke", "#fff");
+  circle = paper.circle(50, 40, 20);
+  circle.attr({
+    fill: '#f00',
+    stroke: "#fff",
+    data: {
+      fichas: 0
+    },
+    cursor: "pointer"
+  }).drag(function(dx, dy) {
+    return this.attr({
+      cx: this.ox + dx,
+      cy: this.oy + dy
+    });
+  }, function() {
+    this.ox = this.attr("cx");
+    return this.oy = this.attr("cy");
+  });
   return $(window).resize(function() {
     return paper.setSize($(window).width(), $(window).height());
   });
