db396e887db6236fc63e08ea3734cca0792ae1df
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2013-07-04 21:00:18 -0300

    Sistema de colisão entre dois objetos

--- assets/javascripts/core.coffee
+++ assets/javascripts/core.coffee
@@ -1,26 +1,106 @@
 
 $ ->
+  in_range = (val, start, size) ->
+    !(val + size < start or val > start + size)
 
-  paper = Raphael($("#main"), "100%", "100%")
+  line_collision = (x1, y1, width1, height1, x2, y2, width2, height2) ->
+   a = {top: y1, bottom: y1+height1, left: x1, right: x1+width1}
+   b = {top: y2, bottom: y2+height2, left: x2, right: x2+width2}
 
-  circle = paper.circle(50, 40, 20)
+   !(a.left >= b.right or a.right <= b.left or
+           a.top >= b.bottom or a.bottom <= b.top)
+  start = () ->
+    @ox = if @type == 'circle' then @attr("cx") else @attr("x")
+    @oy = if @type == 'circle' then @attr("cy") else @attr("y")
+    @attr
+      cursor: "move"
+  end = () ->
+    @attr
+      cursor: "pointer"
+  move = (dx, dy) ->
+    bbox = @getBBox()
 
-  circle
+    set.forEach (e) =>
+      if e.id != @id
+        bbox2 = e.getBBox()
+        # keeps Circle in boarder
+        x = @ox + dx
+
+        y = @oy + dy
+
+        if @type == 'circle'
+          x = x - @attr('r')
+          y = y - @attr('r')
+
+        x = if x < 0 then 0 else (if x > width - bbox.width then width - bbox.width else x)
+
+
+        y = if y < 0 then 0 else (if y > height - bbox.height then height - bbox.height else y)
+
+        #collision system
+        if not line_collision(bbox2.x, bbox2.y, bbox2.width, bbox2.height, x, bbox.y, bbox.width, bbox.height)
+          if (@stuckx and ( not in_range(y, bbox2.y, bbox2.height) or Math.abs(x - bbox.x) < bbox.width)) or not @stuckx
+            if @type == 'circle'
+              @attr
+                cx: x + @attr('r')
+            else
+              @attr
+                x: x
+
+            @pdx = dx
+            @stuckx = false
+        else
+          @stuckx = true
+          if @type == 'circle'
+            @attr
+              cx: if @pdx > dx then bbox2.x + bbox2.width + 1 + @attr('r') else bbox2.x - 1 - @attr('r')
+          else
+            @attr
+              x: if @pdx > dx then bbox2.x + bbox2.width + 1 else bbox2.x - bbox2.width - 1
+
+        if not line_collision(bbox2.x, bbox2.y, bbox2.width, bbox2.height, bbox.x, y, bbox.width, bbox.height)
+          if ((@stucky and ( not in_range(x, bbox2.x, bbox2.width) or Math.abs(y - bbox.y) < bbox.height) ) or not @stucky )
+            if @type == 'circle'
+              @attr
+                cy: y + @attr('r')
+            else
+              @attr
+                y: y
+            @pdy = dy
+            @stucky = false
+        else
+          @stucky = true
+          if @type == 'circle'
+          else
+            @attr
+              y: if @pdy > dy then bbox2.y + bbox2.height + 1 + @attr('r') else bbox2.y - bbox2.height - 1 - @attr('r')
+
+  width = 500
+  height = 500
+
+  paper = Raphael('canvas', 500, 500)
+
+  set = paper.set()
+
+  rectSize = 50
+
+  set.push(paper.rect(100, 100, rectSize, rectSize)
+    .attr
+       fill: "hsb(0, 0, 0)",
+       stroke: "none",
+       opacity: .5,
+       cursor: "move"
+    .drag(move, start, end))
+
+  set.push(paper.circle(50, 100, 20)
     .attr
       fill: '#f00',
+      opacity: .5,
       stroke: "#fff",
       data:
         fichas: 0
       cursor: "pointer"
-    .drag(
-      (dx, dy) ->
-          @attr
-            cx: @ox + dx,
-            cy: @oy + dy
-      () ->
-        @ox = @attr("cx")
-        @oy = @attr("cy")
-    )
+    .drag(move, start, end))
 
   $(window).resize () ->
     paper.setSize($(window).width(),$(window).height())
--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,23 +1,125 @@
 $(function() {
-  var circle, paper;
-  paper = Raphael($("#main"), "100%", "100%");
-  circle = paper.circle(50, 40, 20);
-  circle.attr({
+  var end, height, in_range, line_collision, move, paper, rectSize, set, start, width;
+  in_range = function(val, start, size) {
+    return !(val + size < start || val > start + size);
+  };
+  line_collision = function(x1, y1, width1, height1, x2, y2, width2, height2) {
+    var a, b;
+    a = {
+      top: y1,
+      bottom: y1 + height1,
+      left: x1,
+      right: x1 + width1
+    };
+    b = {
+      top: y2,
+      bottom: y2 + height2,
+      left: x2,
+      right: x2 + width2
+    };
+    return !(a.left >= b.right || a.right <= b.left || a.top >= b.bottom || a.bottom <= b.top);
+  };
+  start = function() {
+    this.ox = this.type === 'circle' ? this.attr("cx") : this.attr("x");
+    this.oy = this.type === 'circle' ? this.attr("cy") : this.attr("y");
+    return this.attr({
+      cursor: "move"
+    });
+  };
+  end = function() {
+    return this.attr({
+      cursor: "pointer"
+    });
+  };
+  move = function(dx, dy) {
+    var bbox,
+      _this = this;
+    bbox = this.getBBox();
+    return set.forEach(function(e) {
+      var bbox2, x, y;
+      if (e.id !== _this.id) {
+        bbox2 = e.getBBox();
+        x = _this.ox + dx;
+        y = _this.oy + dy;
+        if (_this.type === 'circle') {
+          x = x - _this.attr('r');
+          y = y - _this.attr('r');
+        }
+        x = x < 0 ? 0 : (x > width - bbox.width ? width - bbox.width : x);
+        y = y < 0 ? 0 : (y > height - bbox.height ? height - bbox.height : y);
+        if (!line_collision(bbox2.x, bbox2.y, bbox2.width, bbox2.height, x, bbox.y, bbox.width, bbox.height)) {
+          if ((_this.stuckx && (!in_range(y, bbox2.y, bbox2.height) || Math.abs(x - bbox.x) < bbox.width)) || !_this.stuckx) {
+            if (_this.type === 'circle') {
+              _this.attr({
+                cx: x + _this.attr('r')
+              });
+            } else {
+              _this.attr({
+                x: x
+              });
+            }
+            _this.pdx = dx;
+            _this.stuckx = false;
+          }
+        } else {
+          _this.stuckx = true;
+          if (_this.type === 'circle') {
+            _this.attr({
+              cx: _this.pdx > dx ? bbox2.x + bbox2.width + 1 + _this.attr('r') : bbox2.x - 1 - _this.attr('r')
+            });
+          } else {
+            _this.attr({
+              x: _this.pdx > dx ? bbox2.x + bbox2.width + 1 : bbox2.x - bbox2.width - 1
+            });
+          }
+        }
+        if (!line_collision(bbox2.x, bbox2.y, bbox2.width, bbox2.height, bbox.x, y, bbox.width, bbox.height)) {
+          if ((_this.stucky && (!in_range(x, bbox2.x, bbox2.width) || Math.abs(y - bbox.y) < bbox2.height)) || !_this.stucky) {
+            if (_this.type === 'circle') {
+              _this.attr({
+                cy: y + _this.attr('r')
+              });
+            } else {
+              _this.attr({
+                y: y
+              });
+            }
+            _this.pdy = dy;
+            return _this.stucky = false;
+          }
+        } else {
+          _this.stucky = true;
+          if (_this.type === 'circle') {
+
+          } else {
+            return _this.attr({
+              y: _this.pdy > dy ? bbox2.y + bbox2.height + 1 + _this.attr('r') : bbox2.y - bbox2.height - 1 - _this.attr('r')
+            });
+          }
+        }
+      }
+    });
+  };
+  width = 500;
+  height = 500;
+  paper = Raphael('canvas', 500, 500);
+  set = paper.set();
+  rectSize = 50;
+  set.push(paper.rect(100, 100, rectSize, rectSize).attr({
+    fill: "hsb(0, 0, 0)",
+    stroke: "none",
+    opacity: .5,
+    cursor: "move"
+  }).drag(move, start, end));
+  set.push(paper.circle(50, 100, 20).attr({
     fill: '#f00',
+    opacity: .5,
     stroke: "#fff",
     data: {
       fichas: 0
     },
     cursor: "pointer"
-  }).drag(function(dx, dy) {
-    return this.attr({
-      cx: this.ox + dx,
-      cy: this.oy + dy
-    });
-  }, function() {
-    this.ox = this.attr("cx");
-    return this.oy = this.attr("cy");
-  });
+  }).drag(move, start, end));
   return $(window).resize(function() {
     return paper.setSize($(window).width(), $(window).height());
   });
